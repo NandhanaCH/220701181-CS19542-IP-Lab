65fc18bc256c2e4652d1da918775e4725054de4c
NandhanaCH <220701181@rajalakshmi.edu.in>
2024-08-31 14:04:17 +0530

    Add files via upload

---
+++ Ex 08/PHP - Banking Application/customer_insert.php
@@ -0,0 +1,20 @@
+<?php
+include('db_connect.php');
+include('form_validation.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && empty($errors)) {
+    $cid = $_POST["cid"];
+    $cname = $_POST["cname"];
+
+    $sql = "INSERT INTO CUSTOMER (CID, CNAME) VALUES ('$cid', '$cname')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New customer created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+
